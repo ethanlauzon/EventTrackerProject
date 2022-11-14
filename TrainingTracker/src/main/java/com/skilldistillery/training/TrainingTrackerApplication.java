0f306f2a73d6234854b817359c9b4abf54bc50be
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-11-14 08:21:23 -0700

    deploying to server

--- TrainingTracker/src/main/java/com/skilldistillery/training/TrainingTrackerApplication.java
+++ TrainingTracker/src/main/java/com/skilldistillery/training/TrainingTrackerApplication.java
@@ -2,10 +2,17 @@ package com.skilldistillery.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrainingTrackerApplication {
+public class TrainingTrackerApplication extends SpringBootServletInitializer{
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(TrainingTrackerApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingTrackerApplication.class, args);
 	}
